78bf0826c173417436ac043f19ce51359a7130d8
ztifff <153470710+ztifff@users.noreply.github.com>
2025-11-21 22:48:26 +0800

    Change import path for App component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./portfolio/App";
+import App from "./App";
 
 // BOOTSTRAP CSS + JS
 import "bootstrap/dist/css/bootstrap.min.css";
